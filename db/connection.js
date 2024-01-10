9acc9cbfcde7f4d947294cf7e439707ef44a1229
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-01-10 11:52:02 +0000

    adds connection setup

--- db/connection.js
+++ db/connection.js
@@ -2,3 +2,13 @@
  * Create your connection to the DB in this file
  * and remember to export it
  */
+
+const { Pool } = require("pg");
+
+if (process.env.PGDATABASE === undefined) {
+  throw new Error("No PGDATABASE configured");
+}
+
+const pool = new Pool();
+
+module.exports = pool;
